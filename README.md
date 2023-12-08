1884793b3cfa113a467d6ef483f4464c35ec95b7
Dregen-Yor <hgcatqdao@outlook.com>
2023-12-08 09:04:56 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Program-Design-Homework-Server
 程设作业--图书管理系统后端
+
+**成员**：
+
+- 孙更欣
+- 殷良毅
+- 李泳道
+- 于瑞灏
---
+++ src/main/java/com/example/TCPserver.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.net.*;
+import java.io.*;
+public class TCPserver extends Thread{
+    private ServerSocket serverSocket;
+    public Socket socket;
+    public TCPserver(int port)throws IOException{
+        serverSocket=new  ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+    public void run(){
+        while(true){
+            try{
+                socket=serverSocket.accept();
+                System.out.println("连接成功");
+                break;
+            }
+            catch(SocketTimeoutException e){
+                System.out.println("连接超时");
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        TCPserver s=new TCPserver(1503);
+        s.run();
+    }
+}
