1ed4a12849f25f1751a901175e7aab3fa17ff56f
Dregen-Yor <hgcatqdao@outlook.com>
2023-12-14 16:14:36 +0800

    正常显示版本

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -7,12 +7,12 @@ public class Book implements Serializable{
     public int Bookid;
     public String Bookauthor;
     public String Bookaddress;
-    public int Booknumber;
+    public int Bookcount;
     public Book(String Bookname,int Bookid,String Bookauthor,String Bookaddress,int Bookcount){
         this.Bookname=Bookname;
         this.Bookid=Bookid;
         this.Bookauthor=Bookauthor;
         this.Bookaddress=Bookaddress;
-        this.Booknumber=Bookcount;
+        this.Bookcount=Bookcount;
     }
 }
